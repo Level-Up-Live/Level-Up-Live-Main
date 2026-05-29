a36ecc6db75975c870deaad67a2a0fcfe0fd876c
ClementHo612 <clem@leveluplive.com>
2026-05-29 16:30:51 -0400

    seo

--- theme-init.js
+++ theme-init.js
@@ -1,5 +1,23 @@
 (function () {
   'use strict';
+
+  var GA_MEASUREMENT_ID = 'G-XDHF2H7QK9';
+  if (!window.__lulGaInitialized) {
+    window.__lulGaInitialized = true;
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', GA_MEASUREMENT_ID);
+
+    var gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_MEASUREMENT_ID;
+    document.head.appendChild(gaScript);
+  }
+
   var STORAGE_KEY = 'lul-theme';
   function fromMedia() {
     return window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
